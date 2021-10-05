1ef22d11c1a5d68f7246ef56344a1890da890377
Kent Jakobsson <kentegel@gmail.com>
2021-10-05 11:45:59 +0200

    added aboutpage content

--- client-app/src/features/About/AboutPage.tsx
+++ client-app/src/features/About/AboutPage.tsx
@@ -15,6 +15,9 @@ const AboutPage = () => {
             <Grid.Column width={8}>
             <Image src='/assets/826-8263457_react-with-typescript-react.png' />
             </Grid.Column>
+            <Grid.Column width={8}>
+            <Image style={{width:'500px', float: 'right'}} src='/assets/4986ee0b20bd65298d2539f3f4efe05f.png' />
+            </Grid.Column>
         </Grid>
         </Segment>
     )
--- client-app/src/features/About/WhyComponent.tsx
+++ client-app/src/features/About/WhyComponent.tsx
@@ -5,12 +5,36 @@ const WhyComponent = () => {
     return (
             <Segment color='blue'>
           <Header>Why this site<span><Icon name='question circle'/></span></Header>
-          <Divider inverted />
-            This project is all about learning and master the React typeScript Package. 
-            This Site is built upon the princip of "Clean Architecture pattern".
-            This means that we bild the site in parts not realted to each part.
-            We have a API C# backend and a typeScript API center whit a TSX(typeScript)
-            frontend react component view.
+            <Divider inverted />
+                <p>
+                    This project is all about learning and master the React typeScript Package. 
+                    This Site is built upon the princip of "Clean Architecture pattern".
+                    This means that we bild the site in parts not realted to each part.
+                    We have a API C# backend and a typeScript API center whit a TSX(typeScript)
+                    frontend react component view.
+                </p>
+                <Divider inverted />      
+                <Header>
+                    What is it about<span><Icon name='question circle'/></span>
+                </Header>
+                <Divider inverted />
+                    <p>
+                        This project is inspired from a Udemy course.
+                        The goal is to learn C# backend API and TS javaScript API and frontend 
+                        typeScript React. 
+                    </p>
+                    <h5>
+                        Project containes
+                    </h5>
+                    <ul>
+                        <li>C#</li>
+                        <li>React ver.17 in TS</li>
+                        <li>API for backend and fronend</li>
+                        <li>Chat SingalR</li>
+                        <li>Mobex data</li>
+                        <li>Sementic ui</li>
+                        <li>Identity</li>
+                    </ul>
             </Segment>
     )
 }
